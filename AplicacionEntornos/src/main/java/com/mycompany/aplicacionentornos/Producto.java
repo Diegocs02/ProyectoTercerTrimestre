047169c7dc77dc2229c7f289871744d7d30c0262
Diego Castilla Sáez <dcastilla293@ieszaidinvergeles.org>
2022-05-27 09:57:44 +0200

    commit nuevo

--- AplicacionEntornos/src/main/java/com/mycompany/aplicacionentornos/Producto.java
+++ AplicacionEntornos/src/main/java/com/mycompany/aplicacionentornos/Producto.java
@@ -22,12 +22,40 @@ public class Producto {
         this.unidades = unidades;
     }
 
+public double precioConIva(){
+        double numero;
+        numero = precio * 1.21;
+
+        return numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public double getPrecio() {
         return precio;
     }
 
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
     public int getUnidades() {
         return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", precio=" + precio + ", idProducto=" + idProducto + ", unidades=" + unidades + '}';
     }
    
 }
